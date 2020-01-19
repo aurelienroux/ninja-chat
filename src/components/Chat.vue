eb11beb4a89cf2d9675208cd6983173ab02a9d5b
Aurélien Roux <aurelien.roux@aerial.ai>
2020-01-18 20:05:28 -0500

    crate newMessage compnt

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -12,16 +12,21 @@
         </ul>
       </div>
       <div class="card-action">
-        <input type="text" name="" id="" />
+        <NewMessage :name="name" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import NewMessage from '@/components/NewMessage.vue'
+
 export default {
   name: 'Chat',
   props: ['name'],
+  components: {
+    NewMessage
+  },
   data() {
     return {}
   }
